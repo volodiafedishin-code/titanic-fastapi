da2ee66e1663f5d1030f8ea6d9916293fcc97572
Volod <volodia.fedishin@gmail.com>
2026-02-27 20:46:23 +0100

    j2j installed

--- inference/app.py
+++ inference/app.py
@@ -18,8 +18,7 @@ templates = Jinja2Templates(directory=templates_path)
 
 # Завантажуємо твою навчену модель ШІ
 # Переконайся, що файл model.pkl лежить у тій же папці
-model_path=os.path.join(base_path,"..","models","titanic_model.pkl")
-model = joblib.load(model_path)
+model=joblib.load("model")
 
 # 1. ГОЛОВНА СТОРІНКА (показуємо твій крутий дизайн)
 @app.get("/")
